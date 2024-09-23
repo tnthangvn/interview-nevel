93d793502a66b3f9da18d9b3c9c06d577dc5338c
Tran Ngoc Thang <tnthangvn@gmail.com>
2024-09-23 09:02:06 +0700

    [IN] fix image deploy

--- docs/_next/static/chunks/app/layout-ccbb7e0e682a34e9.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1872:function(n,e,u){Promise.resolve().then(u.t.bind(u,7893,23))},7893:function(){}},function(n){n.O(0,[929,844,488,744],function(){return n(n.s=1872)}),_N_E=n.O()}]);
